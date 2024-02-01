42cdbe9348a740ca5e45b4dba48af3370e27531d
jeffreystorer <jeffrey.storer@gmail.com>
2024-02-01 11:54:03 -0500

    2/1: Edit layout to add picture

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,5 +4,19 @@
 
 <main>
 	<Hero />
+    <div id='new-wing'></div>
 	<slot />
 </main>
+
+<style>
+    
+    #new-wing {
+        aspect-ratio: 650/160;
+        background-image: url('$lib/assets/images/NewWing.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        margin: 0, auto;
+        width: 100%;
+    }
+</style>
+
