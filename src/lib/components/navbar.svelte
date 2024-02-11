21edb87aaed02dcb9605fae49295fffeb64d1b4c
jeffreystorer <jeffrey.storer@gmail.com>
2024-02-11 13:50:50 -0500

    2/11: Remove afterNavigation

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,15 +1,6 @@
 <script>
 	import { page } from '$app/stores';
-	import { browser } from '$app/environment';
-	import { invalidateAll, afterNavigate } from '$app/navigation';
-	$: path = $page.url.pathname;
-	if (browser) afterNavigate(invalidateAll());
 	import { pages } from '$lib/constants';
-	import homeIcon from '$lib/assets/icons/homeicon.svg';
-	import aboutIcon from '$lib/assets/icons/abouticon.svg';
-	import newseventsIcon from '$lib/assets/icons/newseventsicon.svg';
-	import newarrivalsIcon from '$lib/assets/icons/newarrivalsicon.svg';
-	import donateIcon from '$lib/assets/icons/donateicon.svg';
 	const icons = {
 		home: '<svg class="icon" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>',
 		about:
